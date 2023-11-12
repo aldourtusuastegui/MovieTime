f11ec2df3df581a3c0009c689ac7e2239e08243c
Aldo Urtusuastegui <aldo.urtu@gmail.com>
2023-11-12 15:33:41 -0700

    displayed popular movies in MoviesFragment

---
+++ app/src/main/java/com/acsoft/movietime/feature_movies/presentation/MovieAdapter.kt
@@ -0,0 +1,48 @@
+package com.acsoft.movietime.feature_movies.presentation
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.acsoft.movietime.databinding.MovieItemBinding
+import com.acsoft.movietime.feature_movies.domain.entities.Movie
+import com.acsoft.movietime.utils.AppConstants
+import com.bumptech.glide.Glide
+
+class MovieAdapter : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    private var moviesList = listOf<Movie>()
+
+    fun setMovieList(movieList: List<Movie>) {
+        this.moviesList = movieList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceViewHolder(itemBinding, parent.context)
+    }
+
+    override fun getItemCount(): Int {
+        return moviesList.size
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when(holder) {
+            is PlaceViewHolder -> {
+                holder.bind(moviesList[position])
+            }
+        }
+    }
+
+    private inner class PlaceViewHolder(val binding : MovieItemBinding, val context: Context) :
+        BaseViewHolder<Movie>(binding.root) {
+        override fun bind(item: Movie) {
+            Glide.with(context)
+                .load("https://image.tmdb.org/t/p/w500/${item.posterPath}")
+                .centerCrop()
+                .into(binding.imgMovie)
+        }
+    }
+}
--- app/src/main/java/com/acsoft/movietime/feature_movies/presentation/MoviesFragment.kt
+++ app/src/main/java/com/acsoft/movietime/feature_movies/presentation/MoviesFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.acsoft.movietime.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.acsoft.movietime.core.Result
 import com.acsoft.movietime.databinding.FragmentMoviesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,20 +15,41 @@ import dagger.hilt.android.AndroidEntryPoint
 class MoviesFragment : Fragment() {
 
     private var _binding: FragmentMoviesBinding? = null
-
     private val binding get() = _binding!!
 
     private val moviesViewModel: MoviesViewModel by viewModels()
+    private lateinit var adapter: MovieAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = MovieAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         moviesViewModel.getPopularMovies()
+
+        binding.rvPopularMovies.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvPopularMovies.adapter = adapter
+
+        moviesViewModel.popularMoviesList.observe(viewLifecycleOwner) { result ->
+            when(result) {
+                is Result.Loading -> {
+                }
+                is Result.Success -> {
+                    adapter.setMovieList(result.data.results ?: listOf())
+                }
+                is Result.Failure -> {
+                }
+            }
+        }
     }
 }
